d60c7e2c5dd911b0bb4149121dc905447a5c1f60
Marcio Junior <marcio.mj70@gmail.com>
2022-12-11 00:02:12 -0300

    feat: auth module

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { User, UsersService } from 'src/users/users.service';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(username: string, pass: string): Promise<User> {
+    const user = await this.usersService.findOne(username);
+    if (user && user.password === pass) {
+      return {
+        userId: user.userId,
+        username: user.username,
+      };
+    }
+    return null;
+  }
+
+  async login(user: User) {
+    const payload = { username: user.username, sub: user.userId };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
